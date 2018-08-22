80a3172b26f3ab7c916f0c32f40b52b3ab7b063a
zenott <mblevel@gmail.com>
2018-08-22 18:34:43 +0200

    Add calculator layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <div id="calc-div">
+          <div id="display">00000</div>
+          <div id="AC" className="buttons">AC</div>
+          <div id="divide" className="buttons">/</div>
+          <div id="multiply" className="buttons">x</div>
+          <div id="seven" className="buttons">7</div>
+          <div id="eight" className="buttons">8</div>
+          <div id="nine" className="buttons">9</div>
+          <div id="minus" className="buttons">-</div>
+          <div id="four" className="buttons">4</div>
+          <div id="five" className="buttons">5</div>
+          <div id="six" className="buttons">6</div>
+          <div id="plus" className="buttons">+</div>
+          <div id="one" className="buttons">1</div>
+          <div id="two" className="buttons">2</div>
+          <div id="three" className="buttons">3</div>
+          <div id="equal" className="buttons">=</div>
+          <div id="zero" className="buttons">0</div>
+          <div id="point" className="buttons">.</div>
+        </div>
       </div>
     );
   }
