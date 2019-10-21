6737130986123a2c9a4e934a8a6bf1a1c4eb33ed
zenott <mblevel@gmail.com>
2019-10-21 22:32:58 +0200

    Updates

---
+++ precache-manifest.23e235e5340ad718c7b44b6844e63d2b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d64d5550a1fe41409fe8b4224fb6c69",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "627f24091dce46cb5197",
+    "url": "/react-calculator/static/js/2.e5d8eb50.chunk.js"
+  },
+  {
+    "revision": "85944dce3f34e8e98c54",
+    "url": "/react-calculator/static/js/main.1cb687af.chunk.js"
+  },
+  {
+    "revision": "5d7efee833cd0745af21",
+    "url": "/react-calculator/static/js/runtime-main.b98b77f1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-calculator/precache-manifest.eded14b9567827c4bce8a92ca81b08a0.js"
+  "/react-calculator/precache-manifest.23e235e5340ad718c7b44b6844e63d2b.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-calculator/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-calculator/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
